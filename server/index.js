83c0a063dead2bd63d44be502e99d801764e7f7f
alexkrewson <alex.krewson@gmail.com>
2022-08-18 21:57:32 -0700

    made many changes

--- server/index.js
+++ server/index.js
@@ -1,20 +1,12 @@
+
 const path = require('path');
 
 const express = require("express");
 
-// const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
-// const app = express();
+const app = express();
 
-
-
-// app.get("/api", (req, res) => {
-//     res.json({ message: "Hello from server!" });
-// });
-
-// app.listen(PORT, () => {
-//     console.log(`Server listening on ${PORT}`);
-// });
 
 
 // Have Node serve the files for our built React app
@@ -22,10 +14,14 @@ app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 // Handle GET requests to /api route
 app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
+  res.json({ message: "Hello from server!" });
 });
 
 // All other GET requests not handled before will return our React app
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
 });
